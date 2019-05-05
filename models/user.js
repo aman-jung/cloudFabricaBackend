d11fb64413c4fbd99ecddd05fa4610bd6a81e427
aman-tunerlabs <aman@tunerlabs.com>
2019-05-05 10:59:47 +0530

    testing

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 module.exports={
     name:"user",
     schema:{
-        name:String,
-        email:String,
-        password:String,
-        type:String,
-        createdAt:Date
+        "name":String,
+        "email":String,
+        "password":String,
+        "type":String,
+        "createdAt":Date,
+        "person of contact":String,
+        "Phone number":Number
     }
 }
