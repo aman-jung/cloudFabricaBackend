26ef3c311fc86c2fbc9a736a679de1a407f77bef
aman-tunerlabs <aman@tunerlabs.com>
2019-05-24 22:30:27 +0530

    code optimize

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,16 +1,13 @@
 define({
-  "name": "Cloud Fabrica API Documentation",
-  "version": "1.0.0",
+  "name": "",
+  "version": "0.0.0",
   "description": "",
-  "template": {
-    "forceLanguage": "en"
-  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-05-07T15:21:19.282Z",
+    "time": "2019-05-24T16:40:06.790Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
