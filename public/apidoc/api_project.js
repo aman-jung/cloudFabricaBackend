2d65a2fab0e85cb22b9184236fc26ab7b79211d1
aman-tunerlabs <aman@tunerlabs.com>
2019-07-20 12:33:12 +0530

    apidoc added

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-07-20T05:48:09.604Z",
+    "time": "2019-07-20T07:02:49.803Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
