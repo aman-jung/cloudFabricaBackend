b8e62d2ce80d6ff47e466b29c8d2848c4ee4af4b
aman-tunerlabs <aman@tunerlabs.com>
2019-07-20 12:29:19 +0530

    added enable rating submissions

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-07-20T04:40:29.161Z",
+    "time": "2019-07-20T05:48:09.604Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
